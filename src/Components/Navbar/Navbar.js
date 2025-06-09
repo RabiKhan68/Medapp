08599f3043bf6527ffa27e69f4f076d9f58793b4
RabiKhan68 <Rabibro063@gmail.com>
2025-06-09 13:16:22 -0400

    initial

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,40 @@
+// src/Components/Navbar/Navbar.js
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css"; // Optional if you have styles
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="nav__logo">
+        <Link to="/">
+          StayHealthy
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="26"
+            width="26"
+            viewBox="0 0 1000 1000"
+            style={{ fill: "#3685fb" }}
+          >
+            <title>Doctor With Stethoscope SVG icon</title>
+            <g>
+              <path d="M499.8,10c91.7,0,166,74.3,166,166c0,91.7-74.3,166-166,166c-91.7,0-166-74.3-166-166C333.8,84.3,408.1,10,499.8,10z" />
+              <path d="M499.8,522.8c71.2,0,129.1-58.7,129.1-129.1H370.6C370.6,464.1,428.6,522.8,499.8,522.8z" />
+              {/* Truncated last path for simplicity */}
+            </g>
+          </svg>
+        </Link>
+        <span>.</span>
+      </div>
+
+      <ul className="nav__links active">
+        <li className="link"><Link to="/">Home</Link></li>
+        <li className="link"><Link to="/appointments">Appointments</Link></li>
+        <li className="link"><Link to="/signup"><button className="btn1">Sign Up</button></Link></li>
+        <li className="link"><Link to="/login"><button className="btn1">Login</button></Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/Pages/Appointments.js
@@ -0,0 +1,6 @@
+// ✅ src/Pages/Home.js
+import React from "react";
+
+export default function Login() {
+  return <h2>Welcome to StayHealthy Home</h2>;
+}
